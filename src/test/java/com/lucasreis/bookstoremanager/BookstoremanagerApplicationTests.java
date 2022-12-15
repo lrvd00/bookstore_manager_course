b235d76464949f629504aa0861c92d89445437eb
Lucas <lucasrvdias@gmail.com>
2022-12-14 21:51:05 -0300

    Correction

--- src/test/java/com/lucasreis/bookstoremanager/BookstoremanagerApplicationTests.java
+++ src/test/java/com/lucasreis/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -1,5 +1,3 @@
-git remote add origin https://github.com/lrvd00/bookstore_manager_course.gitpackage com.lucasreis.bookstoremanager;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
