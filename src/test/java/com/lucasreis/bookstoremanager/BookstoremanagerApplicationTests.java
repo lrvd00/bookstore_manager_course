18a3ae899f1a53538e55a29bf4336f9798ff6254
Lucas <lucasrvdias@gmail.com>
2022-12-14 21:16:05 -0300

    Added system.properties to add Java 19

--- src/test/java/com/lucasreis/bookstoremanager/BookstoremanagerApplicationTests.java
+++ src/test/java/com/lucasreis/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.lucasreis.bookstoremanager;
+git remote add origin https://github.com/lrvd00/bookstore_manager_course.gitpackage com.lucasreis.bookstoremanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
