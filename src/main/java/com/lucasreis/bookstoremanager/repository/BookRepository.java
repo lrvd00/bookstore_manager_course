7f0a75dcc50bf6570794eb4742b065fcfc5afc00
Lucas <lucasrvdias@gmail.com>
2022-12-19 22:15:31 -0300

    BookRepositoryClass

---
+++ src/main/java/com/lucasreis/bookstoremanager/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.lucasreis.bookstoremanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
